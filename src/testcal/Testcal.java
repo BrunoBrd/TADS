a325a8b8d1c5ef5516c614ab557bd68b308c3321
Bruno <Bruno@BRD-7VIK44G>
2017-03-29 20:38:23 -0400

    Dia 29/03

---
+++ src/testcal/Testcal.java
@@ -0,0 +1,64 @@
+
+package testcal;
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Bruno
+ */
+public class Testcal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+   
+        String imprimir;
+        float primeiroOperador, segundoOperador;
+        String operacao;
+        
+        
+        Scanner entrada = new Scanner(System.in);
+        
+        JOptionPane.showInputDialog("Escolha sua operação [+ - * / ]: ");
+        operacao = entrada.nextLine();
+        
+        JOptionPane.showInputDialog("Entre com o primeiro número: ");
+        primeiroOperador = entrada.nextFloat();
+        
+        JOptionPane.showInputDialog("Entre com o segundo número: ");
+        segundoOperador = entrada.nextFloat();
+        
+        JOptionPane.showMessageDialog(null,"O resultado e :" +operacao,"Resultado", JOptionPane.INFORMATION_MESSAGE);
+        System.out.println();
+        
+        switch( operacao.charAt(0) )
+        {
+            case '+':
+                System.out.printf("%.2f + %.2f = %.2f", primeiroOperador, segundoOperador, primeiroOperador + segundoOperador);
+                break;        
+                
+            case '-':
+                System.out.printf("%.2f - %.2f = %.2f", primeiroOperador, segundoOperador, primeiroOperador - segundoOperador);
+                break;
+                
+            case '*':
+                System.out.printf("%.2f * %.2f = %.2f", primeiroOperador, segundoOperador, primeiroOperador * segundoOperador);
+                break;
+                
+            case '/':
+                System.out.printf("%.2f / %.2f = %.2f", primeiroOperador, segundoOperador, primeiroOperador / segundoOperador);
+                break;
+            
+            default:
+                System.out.printf("Você digitou uma operação inválida.");        
+            
+        }
+        
+
+    }
+}
+
